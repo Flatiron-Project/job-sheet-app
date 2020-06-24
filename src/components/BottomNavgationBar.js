ad45cc29decea723738d2dbe58d178c63064eccb
Andrew Davisson <akdavisson4@gmail.com>
2020-06-24 07:05:57 -0600

    added bottom navigation component

---
+++ src/components/BottomNavgationBar.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const BottomNavigationBar = () => {
+  const [ active, setActive ] = useState('')
+
+  return (
+    <BottomNavigation active={this.state.active} hidden={false} >
+      <BottomNavigation.Action
+          key="today"
+          icon="today"
+          label="Today"
+          onPress={() => setActive('today')}
+      />
+      <BottomNavigation.Action
+          key="people"
+          icon="people"
+          label="People"
+          onPress={() => setActive('people')}
+      />
+      <BottomNavigation.Action
+          key="bookmark-border"
+          icon="bookmark-border"
+          label="Bookmark"
+          onPress={() => setActive('bookmark-border')}
+      />
+      <BottomNavigation.Action
+          key="settings"
+          icon="settings"
+          label="Settings"
+          onPress={() => setActive('settings')}
+      />
+    </BottomNavigation>
+  );
+}
+ 
+export default BottomNavigationBar;
