e4435f6823bdd0f4a6defa21ca8c010183a9458b
Andrew Davisson <akdavisson4@gmail.com>
2020-06-24 07:07:03 -0600

    import BottomNavigation

--- src/components/BottomNavgationBar.js
+++ src/components/BottomNavgationBar.js
@@ -1,36 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react'
+import BottomNavigation from 'react-native-material-ui'
 
 const BottomNavigationBar = () => {
-  const [ active, setActive ] = useState('')
+  const [active, setActive] = useState('')
 
   return (
-    <BottomNavigation active={this.state.active} hidden={false} >
+    <BottomNavigation active={active} hidden={false}>
       <BottomNavigation.Action
-          key="today"
-          icon="today"
-          label="Today"
-          onPress={() => setActive('today')}
+        key='today'
+        icon='today'
+        label='Today'
+        onPress={() => setActive('today')}
       />
       <BottomNavigation.Action
-          key="people"
-          icon="people"
-          label="People"
-          onPress={() => setActive('people')}
+        key='people'
+        icon='people'
+        label='People'
+        onPress={() => setActive('people')}
       />
       <BottomNavigation.Action
-          key="bookmark-border"
-          icon="bookmark-border"
-          label="Bookmark"
-          onPress={() => setActive('bookmark-border')}
+        key='bookmark-border'
+        icon='bookmark-border'
+        label='Bookmark'
+        onPress={() => setActive('bookmark-border')}
       />
       <BottomNavigation.Action
-          key="settings"
-          icon="settings"
-          label="Settings"
-          onPress={() => setActive('settings')}
+        key='settings'
+        icon='settings'
+        label='Settings'
+        onPress={() => setActive('settings')}
       />
     </BottomNavigation>
-  );
+  )
 }
- 
-export default BottomNavigationBar;
+
+export default BottomNavigationBar
