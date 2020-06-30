689cd9a85db74be9bb0dc4e9a8498e3c90d47bbd
Andrew Davisson <akdavisson4@gmail.com>
2020-06-29 22:37:54 -0600

    linear gradient now works

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native'
+import { LinearGradient } from 'expo-linear-gradient'
 
 const Login = () => {
   return (
-    <View >
-      <Text>Login</Text>
+    <View style={styles.loginView}>
+      <LinearGradient
+        colors={['#6200EE', '#03DAC5']}
+        style={styles.linearGradient}
+      >
+        <Text>Login</Text>
+      </LinearGradient>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  
+  loginView: {
+    flex: 1,
+  },
+  linearGradient: {
+    flex: 1,
+  }
 })
  
 export default Login;
